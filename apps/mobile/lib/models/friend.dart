befee01ba04c0da675f2029278706f9c10246cf5
Pd0ubleU <preawa.ya2548@gmail.com>
2026-05-07 11:46:31 +0700

    PWforntendV1
    frontend for integrate with github first

---
+++ apps/mobile/lib/models/friend.dart
@@ -0,0 +1,33 @@
+class Friend {
+  String name;           // editable note/nickname you set for this friend
+  final String username; // their actual account username
+  final String lastMessage;
+  final bool isOnline;
+  int unreadCount;
+  final bool isInRoom;
+  final String avatar;
+  final String interest;
+
+  Friend({
+    required this.name,
+    required this.username,
+    required this.lastMessage,
+    required this.isOnline,
+    this.unreadCount = 0,
+    required this.isInRoom,
+    this.avatar = '',
+    this.interest = '',
+  });
+}
+
+class ChatMessage {
+  final String text;
+  final bool isMe;
+  final String time;
+
+  const ChatMessage({
+    required this.text,
+    required this.isMe,
+    required this.time,
+  });
+}
